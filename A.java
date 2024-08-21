f50a42fc3c9ac5e7457912a65dac3059e4dfcdd9
KajalRathod05 <kajalrathod0576@gmail.com>
2024-08-21 20:41:04 +0530

    By Kajal

---
+++ A.java
@@ -0,0 +1,13 @@
+public class A
+{
+
+   public void m1()
+   {
+    System.out.println("A-m1");
+   }
+  public static void main(String args[])
+  {
+    A a=new A();
+     a.m1();
+  }
+}
---
+++ B.java
@@ -0,0 +1,13 @@
+public class B
+{
+
+   public void m1()
+   {
+    System.out.println("B-m1");
+   }
+  public static void main(String args[])
+  {
+    B b=new B();
+     b.m1();
+  }
+}
